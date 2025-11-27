a29727b916b9c206626bbbd5e9514ce955c6e750
acemaiyeu <nthanhhuy82@gmail.com>
2025-11-27 17:28:17 +0700

    fix

--- src/pages/Client/Echo.js
+++ src/pages/Client/Echo.js
@@ -1,5 +1,6 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+import { MAIN_DOMAIN } from '../const/const';
 
 // Gán Pusher vào window
 window.Pusher = Pusher;
@@ -12,7 +13,8 @@ const echo = new Echo({
     cluster: 'mt1', 
     
     // 👇 RẤT QUAN TRỌNG: Cấu hình để sử dụng HTTP/WS
-    wsHost: '127.0.0.1', 
+    // wsHost: '127.0.0.1', 
+    wsHost: `${MAIN_DOMAIN ?? '127.0.0.1'}`, // Thay thế {MAIN_DOMAIN} bằng tên miền chính của bạn
     wsPort: 6001,       
     
     // 👇 Cần đặt là FALSE để sử dụng giao thức WS (không bảo mật)
--- src/pages/const/const.js
+++ src/pages/const/const.js
@@ -1,3 +1,4 @@
 // export const API_URL = "http://192.168.1.108:8000/api";
 export const API_URL = "https://themeforstudents.io.vn/api";
 export const auth = "Almo"
+export const MAIN_DOMAIN = "https://themeforstudents.io.vn";
