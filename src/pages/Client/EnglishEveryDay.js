cc61293df827d62d8ef11a20d457815a48ea814c
acemaiyeu <nthanhhuy82@gmail.com>
2026-01-04 19:48:13 +0700

    update v21

--- src/pages/Client/EnglishEveryDay.js
+++ src/pages/Client/EnglishEveryDay.js
@@ -10,11 +10,34 @@ class EnglishEveryDay extends React.Component {
         this.getVocabularyBox()
     }
     getVocabularyBox = () => {
-        axios.get(`${API_URL}/question/7338?limit=1`).then((res) => {
+        let quote = JSON.parse(localStorage.getItem("quotes"));
+        let call_api_again = true;
+        if(quote){
+            if(quote.time == new Date().toISOString().split('T')[0]){
+                 call_api_again = false;
+            }
+        }
+        if(call_api_again){
+            axios.get(`${API_URL}/quotes-random`).then((res) => {
+                console.log(res.data.data.title)
+                this.setState({
+                    messagage: res.data.data.title + " => " + res.data.data.mean
+                })
+                let ob = {
+                    title: res.data.data.title,
+                    mean: res.data.data.mean,
+                    time: new Date().toISOString().split('T')[0]
+                }
+                localStorage.setItem("quotes", JSON.stringify(ob))
+            }).catch((e) => {
+                    console.log(e)
+                })
+        }else{
             this.setState({
-                messagage: res.data.data[0].title_english
-            })
-        })
+                    messagage: quote.title + " => " + quote.mean
+                })
+        }
+        
     }
     render() {
         let { messagage }   = this.state;
